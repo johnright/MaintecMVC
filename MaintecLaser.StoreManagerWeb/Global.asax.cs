59b9c1408ee69c472b80d4ca9b92d0d8dd5cd8e0
John Right <giusto.gnn@gmail.com>
2016-05-19 02:12:06 +0200

    test

--- MaintecLaser.StoreManagerWeb/Global.asax.cs
+++ MaintecLaser.StoreManagerWeb/Global.asax.cs
@@ -12,7 +12,7 @@ namespace MaintecLaser.StoreManagerWeb
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes); 
+            RouteConfig.RegisterRoutes(RouteTable.Routes);  
         }
     }
 }
